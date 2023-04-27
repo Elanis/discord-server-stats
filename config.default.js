2564e125084fda9cc92aa3aac61f73b007a5b59e
Elanis <elanis@hotmail.com>
2023-04-27 17:59:19 +0200

    Exporting in the example would be better

--- config.default.js
+++ config.default.js
@@ -1,6 +1,6 @@
-const guilds = {
+export const guilds = {
 	'Name': 10000000000000000
 };
 
-const botToken = 'BOT TOKEN HERE';
-const minDate = new Date(2001, 0, 1); // January 1st 2001
+export const botToken = 'BOT TOKEN HERE';
+export const minDate = new Date(2001, 0, 1); // January 1st 2001
