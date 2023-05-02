ccf52face7fb3fb47c44decdefb67cb755cb2477
Elanis <elanis@hotmail.com>
2023-05-02 11:59:38 +0200

    Post top users

--- app.js
+++ app.js
@@ -222,6 +222,15 @@ async function channelInfo(interaction) {
 	const toStr = interaction.options.getString('to');
 	const to = toStr === null ? new Date(Date.now()) : new Date(interaction.options.getString('to'));
 
+	// Load users cache
+	let users = {};
+	const userCacheFile = `data/${interaction.guildId}/users.json`;
+	try {
+		const usersListStr = await readFile(userCacheFile, 'utf-8');
+		users = JSON.parse(usersListStr);
+	} catch(e) { /* Do not exists: default value */ }
+
+	// Load messages
 	let messages = [];
 	let messagesCacheFile = `data/${interaction.guildId}/messages-${channel.id}.json`
 	try {
@@ -230,6 +239,24 @@ async function channelInfo(interaction) {
 	} catch(e) { console.error(e); /* Do not exists: default value */ }
 
 	const filteredMessages = messages.filter(x => from.getTime() <= x.createdTimestamp && x.createdTimestamp <= to.getTime());
+	const topUsersObj = {};
+	for(const filteredMessage of filteredMessages) {
+		if(!topUsersObj[filteredMessage.author]) {
+			topUsersObj[filteredMessage.author] = 0;
+		}
+
+		topUsersObj[filteredMessage.author]++;
+	}
+	const topUsers = [];
+	for(const userId in topUsersObj) {
+		topUsers.push({
+			id: userId,
+			name: users[userId].username + '#' + users[userId].discriminator,
+			amount: topUsersObj[userId],
+		})
+	}
+
+	topUsers.sort((a, b) => b.amount - a.amount);
 
 	const exampleEmbed = new EmbedBuilder()
 		.setColor(0x0099FF)
@@ -241,6 +268,7 @@ async function channelInfo(interaction) {
 		.addFields(
 			{ name: 'Messages:', value: filteredMessages.length.toString(), inline: true },
 			{ name: 'Users', value: Array.from(new Set(filteredMessages.map(x => x.author))).length.toString(), inline: true },
+			{ name: 'Top Users', value: '```\n' + topUsers.filter((x, i) => i < 10).map((x, i) => `#${i + 1} - ${x.name} - ${x.amount} messages`).join('\n') + '\n```' }
 		)
 		.setTimestamp();
 
