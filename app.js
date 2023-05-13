6ec2968c29f0845cd3a9576fc0f5d3e975aa0ae5
Elanis <elanis@hotmail.com>
2023-05-13 15:05:13 +0200

    Refacto + Performance improvements + plot top users activities as well

--- app.js
+++ app.js
@@ -41,6 +41,158 @@ async function getTextChannelsForGuild(id) {
 	};
 }
 
+function getMinMaxDatesFromMessageList(messages) {
+	let minDate = Number.MAX_SAFE_INTEGER;
+	let maxDate = Number.MIN_SAFE_INTEGER;
+	for(const message of messages) {
+		if(minDate > message.createdTimestamp) {
+			minDate = message.createdTimestamp
+		}
+
+		if(maxDate < message.createdTimestamp) {
+			maxDate = message.createdTimestamp
+		}
+	}
+
+	return { minDate, maxDate };
+}
+
+function getDatesListFromMessageList(messages) {
+	const { minDate, maxDate } = getMinMaxDatesFromMessageList(messages);
+	const dates = [];
+	for(let date = minDate; date <= maxDate; date += 24 * 60 * 60 * 1000) {
+		dates.push(
+			getDateFromDateTime(new Date(date))
+		);
+	}
+
+	return dates;
+}
+
+function getChannelInfoMessagesPerDayChart(filteredMessages) {
+	const dates = getDatesListFromMessageList(filteredMessages);
+
+	const chart = ChartJSImage().chart({
+		"type": "line",
+		"data": {
+			"labels": dates,
+			"datasets": [
+				{
+					"label": "Messages per day",
+					"borderColor": "rgb(255,+99,+132)",
+					"backgroundColor": "rgba(255,+99,+132,+.5)",
+					"data": dates.map(day => filteredMessages.filter(x => getDateFromDateTime(new Date(x.createdTimestamp)) === day).length),
+				}
+			]
+		},
+		"options": {
+			"title": {
+				"display": false,
+				"text": "Messages per day"
+			},
+			"scales": {
+				"xAxes": [
+					{
+						"scaleLabel": {
+							"display": false,
+							"labelString": "Day"
+						}
+					}
+				],
+				"yAxes": [
+					{
+						"stacked": true,
+						"scaleLabel": {
+							"display": true,
+							"labelString": "Messages"
+						}
+					}
+				]
+			}
+		}
+	}) // Line chart
+	.backgroundColor('white')
+	.width(400 + dates.length * 4)
+	.height(300); // 300px
+
+	return chart;
+}
+
+function getChannelInfoUserMessagesPerDayChart(filteredMessages, topUsers) {
+	const dates = getDatesListFromMessageList(filteredMessages);
+	const colors = ['#1abc9c', '#f1c40f', '#2ecc71', '#e67e22', '#3498db', '#e74c3c', '#9b59b6', '#34495e', '#95a5a6', '#e84393'];
+	
+	const chart = ChartJSImage().chart({
+		"type": "line",
+		"data": {
+			"labels": dates,
+			"datasets": 
+				topUsers.map((user, index) => ({
+					label: user.name,
+					"borderColor": colors[index],
+					"backgroundColor": colors[index],
+					data: dates.map(day => 
+						filteredMessages.filter(x => x.author === user.id && getDateFromDateTime(new Date(x.createdTimestamp)) === day).length
+					),
+				}))
+		},
+		"options": {
+			"title": {
+				"display": false,
+				"text": "Messages per day"
+			},
+			"scales": {
+				"xAxes": [
+					{
+						"scaleLabel": {
+							"display": false,
+							"labelString": "Day"
+						}
+					}
+				],
+				"yAxes": [
+					{
+						"stacked": true,
+						"scaleLabel": {
+							"display": true,
+							"labelString": "Messages"
+						}
+					}
+				]
+			}
+		}
+	}) // Line chart
+	.backgroundColor('white')
+	.width(400 + dates.length * 4)
+	.height(300); // 300px
+
+	return chart;
+}
+
+function getTopUsersFromMessages(messages, users) {
+	const topUsersObj = {};
+	for(const message of messages) {
+		if(!topUsersObj[message.author]) {
+			topUsersObj[message.author] = 0;
+		}
+
+		topUsersObj[message.author]++;
+	}
+
+	const topUsers = [];
+	for(const userId in topUsersObj) {
+		topUsers.push({
+			id: userId,
+			name: users[userId].username + '#' + users[userId].discriminator,
+			amount: topUsersObj[userId],
+		})
+	}
+
+	topUsers.sort((a, b) => b.amount - a.amount);
+
+	return topUsers;
+}
+
 const commandsList = [{
 	name: 'channelinfo',
 	description: 'Get Channel informations for a specific period',
@@ -222,7 +374,7 @@ async function channelInfo(interaction) {
 	const channel = interaction.options.getChannel('channel');
 
 	if(channel === null) {
-		await interaction.reply({ content: 'Invalid channel !' });
+		await interaction.editReply({ content: 'Invalid channel !' });
 	}
 
 	const fromStr = interaction.options.getString('from');
@@ -252,97 +404,42 @@ async function channelInfo(interaction) {
 		return await interaction.editReply({ content: `No data for channel "${channel.name}" between ${getDateFromDateTime(from)} and ${getDateFromDateTime(to)}` });
 	}
 
-	const topUsersObj = {};
-	for(const filteredMessage of filteredMessages) {
-		if(!topUsersObj[filteredMessage.author]) {
-			topUsersObj[filteredMessage.author] = 0;
-		}
+	const topUsers = getTopUsersFromMessages(filteredMessages, users);
+	const top10Users = topUsers.filter((x, i) => i < 10);
 
-		topUsersObj[filteredMessage.author]++;
-	}
-	const topUsers = [];
-	for(const userId in topUsersObj) {
-		topUsers.push({
-			id: userId,
-			name: users[userId].username + '#' + users[userId].discriminator,
-			amount: topUsersObj[userId],
-		})
-	}
+	const globalChart = getChannelInfoMessagesPerDayChart(filteredMessages);
+	const globalFileName = `${interaction.guildId}-${channel.id}-global.png`;
+	const globalFile = new AttachmentBuilder(await globalChart.toBuffer(), { name: globalFileName });
 
-	topUsers.sort((a, b) => b.amount - a.amount);
+	const usersChart = getChannelInfoUserMessagesPerDayChart(filteredMessages, top10Users);
+	const usersFileName = `${interaction.guildId}-${channel.id}-users.png`;
+	const userFile = new AttachmentBuilder(await usersChart.toBuffer(), { name: usersFileName });
 
-	const minDate = Math.min(...filteredMessages.map(x => x.createdTimestamp));
-	const maxDate = Math.max(...filteredMessages.map(x => x.createdTimestamp));
-	const dates = [];
-	for(let date = minDate; date <= maxDate; date += 24 * 60 * 60 * 1000) {
-		dates.push(
-			getDateFromDateTime(new Date(date))
-		);
-	}
+	const { minDate, maxDate } = getMinMaxDatesFromMessageList(filteredMessages);
 
-	const chart = ChartJSImage().chart({
-		"type": "line",
-		"data": {
-			"labels": dates,
-			"datasets": [
-				{
-					"label": "Messages per day",
-					"borderColor": "rgb(255,+99,+132)",
-					"backgroundColor": "rgba(255,+99,+132,+.5)",
-					"data": dates.map(day => filteredMessages.filter(x => getDateFromDateTime(new Date(x.createdTimestamp)) === day).length),
-				}
-			]
-		},
-		"options": {
-			"title": {
-				"display": false,
-				"text": "Messages per day"
-			},
-			"scales": {
-				"xAxes": [
-					{
-						"scaleLabel": {
-							"display": false,
-							"labelString": "Day"
-						}
-					}
-				],
-				"yAxes": [
-					{
-						"stacked": true,
-						"scaleLabel": {
-							"display": true,
-							"labelString": "Messages"
-						}
-					}
-				]
-			}
-		}
-	}) // Line chart
-	.backgroundColor('white')
-	.width(400 + dates.length * 4)
-	.height(300); // 300px
-
-	const fileName = `${interaction.guildId}-${channel.id}.png`;
-
-	const file = new AttachmentBuilder(await chart.toBuffer(), { name: fileName });
-
-	const messageEmbed = new EmbedBuilder()
-		.setColor(0x0099FF)
-		.setTitle(`<#${channel.id}>`)
-		.setImage('attachment://' + fileName)
-		.addFields(
-			{ name: 'From:', value: getDateFromDateTime(new Date(Math.min(...filteredMessages.map(x => x.createdTimestamp)))) },
-			{ name: 'To:', value: getDateFromDateTime(new Date(Math.max(...filteredMessages.map(x => x.createdTimestamp)))) },
-		)
-		.addFields(
-			{ name: 'Messages:', value: filteredMessages.length.toString(), inline: true },
-			{ name: 'Users', value: Array.from(new Set(filteredMessages.map(x => x.author))).length.toString(), inline: true },
-			{ name: 'Top Users', value: '```\n' + topUsers.filter((x, i) => i < 10).map((x, i) => `#${i + 1} - ${x.name} - ${x.amount} messages`).join('\n') + '\n```' }
-		)
-		.setTimestamp();
-
-	await interaction.editReply({ embeds: [messageEmbed], files: [file] });
+	const url = `https://discord.com/channels/${interaction.guildId}/${channel.id}`;
+	const messageEmbeds = [
+		new EmbedBuilder()
+			.setURL(url)
+			.setColor(0x0099FF)
+			.setTitle(`<#${channel.id}>`)
+			.setImage('attachment://' + globalFileName)
+			.addFields(
+				{ name: 'From:', value: getDateFromDateTime(new Date(minDate)) },
+				{ name: 'To:', value: getDateFromDateTime(new Date(maxDate)) },
+			)
+			.addFields(
+				{ name: 'Messages:', value: filteredMessages.length.toString(), inline: true },
+				{ name: 'Users', value: Array.from(new Set(filteredMessages.map(x => x.author))).length.toString(), inline: true },
+				{ name: 'Top Users', value: '```\n' + topUsers.filter((x, i) => i < 10).map((x, i) => `#${i + 1} - ${x.name} - ${x.amount} messages`).join('\n') + '\n```' }
+			)
+			.setTimestamp(),
+		new EmbedBuilder()
+			.setURL(url)
+			.setColor(0x0099FF)
+			.setImage('attachment://' + usersFileName)
+	];
+	await interaction.editReply({ embeds: messageEmbeds, files: [globalFile, userFile] });
 }
 
 client.on('interactionCreate',  async(interaction) => {
